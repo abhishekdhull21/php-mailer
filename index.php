6f4d320d47a03d85c6abd2fd919d13c29ab32c7a
Abhishek Dhull <abhishek.dhull@affluencesolutions.com>
2022-12-28 19:59:53 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+
+  require 'vendor/autoload.php';
+
+$mail = new PHPMailer; //From email address and name 
+$mail->From = "from@yourdomain.com"; 
+$mail->FromName = "Full Name"; //To address and name 
+$mail->addAddress("s@gmail.com", "Recepient Name");//Recipient name is optional
+// $mail->addAddress("recepient1@example.com"); //Address to which recipient will reply 
+// $mail->addReplyTo("reply@yourdomain.com", "Reply"); //CC and BCC 
+// $mail->addCC("cc@example.com"); 
+// $mail->addBCC("bcc@example.com"); //Send HTML or Plain Text email 
+$mail->isHTML(true); 
+$mail->Subject = "Subject Text"; 
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content"; 
+if(!$mail->send()) 
+{
+echo "Mailer Error: " . $mail->ErrorInfo; 
+} 
+else { echo "Message has been sent successfully"; 
+}
+if(!$mail->send()) 
+{ 
+echo "Mailer Error: " . $mail->ErrorInfo; 
+} 
+else 
+{ 
+echo "Message has been sent successfully"; 
+}
